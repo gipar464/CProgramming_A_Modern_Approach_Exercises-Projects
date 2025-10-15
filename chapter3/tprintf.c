49e9294395f2268865a04a89ab80c5ac0fb340be
LuluVnB <paraguya.gian@gmail.com>
2025-10-15 13:02:06 -0700

    wanted to try out the alert escape sequence. didn't work haha

--- chapter3/tprintf.c
+++ chapter3/tprintf.c
@@ -9,8 +9,8 @@ int main(void){
     i = 40;
     x = 839.21f;
 
-    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
-    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+    printf("|%d|%5d|%-5d|%5.3d|\n\a", i, i, i, i);
+    printf("|%10.3f|%10.3e|%-10g|\n\a", x, x, x);
 
     return 0;
 }
