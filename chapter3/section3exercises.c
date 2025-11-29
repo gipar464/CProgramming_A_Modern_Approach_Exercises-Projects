7b14cc3245bee557faff4703103aec8d9c1ba98a
LuluVnB <paraguya.gian@gmail.com>
2025-11-29 00:18:32 -0800

    compiled and executed exercise 1

---
+++ chapter3/section3exercises.c
@@ -0,0 +1,11 @@
+/*Exercises for section 3
+  11/28/2025*/
+
+#include <stdio.h>
+
+int main(void){
+    printf("%6d, %4d\n", 86, 1040);  //prediction: it will print 86000, 1040 
+    printf("%12.5e\n", 30.253);      //prediction: no idea
+    printf("%.4f\n", 83.162);        //prediction: 83.1620
+    printf("%-6.2g\n", .0000009979); //prediction: no idea
+}
