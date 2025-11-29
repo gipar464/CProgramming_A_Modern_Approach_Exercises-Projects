48d3c7093943e3db5e04009c341055897fa5271c
LuluVnB <paraguya.gian@gmail.com>
2025-11-29 13:18:45 -0800

    initialized 3programmingProject2.c

---
+++ chapter3/3programmingProject2.c
@@ -0,0 +1,21 @@
+/*Gian Paraguya, 11/29/2025*/
+
+
+#include <stdio.h>
+
+int main(void){
+    
+    int itemNum, unitPrice, month, day, year;
+
+    printf("Enter item number:\n ");
+    scanf("%d", &itemNum);
+
+    printf("Enter unit price:\n ");
+    scanf("%d", &unitPrice);
+
+    printf("Enter purchase date (mm/dd/yyyy):\n ");
+    scanf("%d/%d/%d", &month, &day, &year);
+
+    
+
+}
