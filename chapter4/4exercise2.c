7a72ee48e098d123afa00b0eed1d48c0b3172582
LuluVnB <paraguya.gian@gmail.com>
2025-12-05 23:09:25 -0800

    completed exercise 4.2

--- chapter4/4exercise2.c
+++ chapter4/4exercise2.c
@@ -3,6 +3,13 @@
 
 /*
 
-If i and j are positive integers, 
+If i and j are positive integers, does (-i)/j always have the same value as -(i/j)? 
+Justify your answer.
+
+No, they do not always have the same value because when you use the uniary operator on 
+the variable i, then i is already negative, so you're dividing negative i by j, which 
+the c compiler will return a truncated version such that the answer is closest to 0.
+On the other hand, evaluating positive i divided by positive j will return a truncated 
+answer, with the negative applied only afterwards. 
 
 */
