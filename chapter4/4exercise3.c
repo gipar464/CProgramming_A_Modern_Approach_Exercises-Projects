ad1558e35c13d837616888012d5f47c8ff722f58
LuluVnB <paraguya.gian@gmail.com>
2025-12-06 19:59:21 -0800

    completed exercise 3

---
+++ chapter4/4exercise3.c
@@ -0,0 +1,18 @@
+//Gian Paraguya, 12/06/2025
+
+/*
+
+What is the value of each of trhe following expressions in C89? 
+Give all possible values if an expression may have more than one value.
+
+(a) 8 / 5
+(b) -8 / 5
+(c) 8 / -5
+(d) -8 / -5
+
+Answers:
+a) 1
+b) -1 or -2
+c) -1 or -2
+d) 1 or 2
+*/
