486f8d5f7a127edbeda0ec52ff28ca3edcdf6399
LuluVnB <paraguya.gian@gmail.com>
2025-12-08 08:50:21 -0800

    completed programming project 2

---
+++ chapter5/5programmingProject2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+    int uHours, minutes; //uHours = user Hours/Mins
+    int rHours; //rHours/Mins = result Hours
+
+    printf("Enter a 24-hour time: ");
+    scanf("%d:%d", &uHours, &minutes);
+
+    if(uHours > 12 && uHours < 25){
+        rHours = uHours - 12;
+    } 
+    else if(uHours <= 12 && uHours > 0){
+        rHours = uHours;
+    } 
+    else{
+        printf("Invalid time. (Use 24-hour clock.)");
+    }
+
+    printf("Equivalent 12-hour time: %d:%d", rHours, minutes);
+
+
+    return 0;
+}
