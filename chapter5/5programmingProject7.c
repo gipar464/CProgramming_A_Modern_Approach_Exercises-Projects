f16a20069eb0733db3244398bf9f4818c2498a6d
LuluVnB <paraguya.gian@gmail.com>
2025-12-08 19:34:22 -0800

    initialized min and max before evaluating all the if statements

--- chapter5/5programmingProject7.c
+++ chapter5/5programmingProject7.c
@@ -7,10 +7,17 @@ to solve this problem is 4. I, however, only managed to do 5.
 #include <stdio.h>
 
 int main(void){
-    int u1, u2, u3, u4, max, min; //u1, u2, u3, u4 = user numbers
+    int u1, u2, u3, u4, min, max; //u1, u2, u3, u4 = user numbers
+
 
     printf("Enter four integers: ");
     scanf("%d %d %d %d", &u1, &u2, &u3, &u4);
+    
+    /*setting min and max to u1 and u2, respectively, must come first before all
+    the if statements because there's a case in which u1 is not less than u2, so the first
+    if statement is never evaluated*/
+    min = u1;
+    max = u2;
 
     if(u1 < u2){
         min = u1;
@@ -23,10 +30,10 @@ int main(void){
         min = u4;
     }
     if(u3 > max){
-        u3 = max;
+        max = u3;
     }
     if(u4 > max){
-        u4 = max;
+        max = u4; 
     }
 
     printf("Largest: %d\n", max);
@@ -34,6 +41,8 @@ int main(void){
 
     return 0;
 }
+
+
 
 
 
