400de8854516b85e80d7134e23bbc020829f18c2
LuluVnB <paraguya.gian@gmail.com>
2025-12-10 01:28:02 -0800

    completed programming project 11

--- chapter5/5programmingProject11.c
+++ chapter5/5programmingProject11.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
 int main(void){
+    int nfd, nsd; //stands number first digit and number second digit, respectively
 
+    printf("Enter a two-digit number: ");
+    scanf("%1d%1d", &nfd, &nsd);
+
+    switch(nfd){
+        case 0: printf("zero-"); break;
+        case 1: printf("one-"); break;
+        case 2: printf("two-"); break;
+        case 3: printf("three-"); break;
+        case 4: printf("four-"); break;
+        case 5: printf("five-"); break;
+        case 6: printf("six-"); break;
+        case 7: printf("seven-"); break;
+        case 8: printf("eight-"); break;
+        case 9: printf("nine-"); break;
+    }
+
+    switch(nsd){
+        case 0: printf("zero"); break;
+        case 1: printf("one"); break;
+        case 2: printf("two"); break;
+        case 3: printf("three"); break;
+        case 4: printf("four"); break;
+        case 5: printf("five"); break;
+        case 6: printf("six"); break; 
+        case 7: printf("seven"); break;
+        case 8: printf("eight"); break; 
+        case 9: printf("nine"); break;
+    }
 
     return 0;
 }
