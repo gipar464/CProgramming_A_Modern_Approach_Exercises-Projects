f9ead8889082c56f3a2a2429289705b3ce525ee0
LuluVnB <paraguya.gian@gmail.com>
2025-12-10 18:54:23 -0800

    sum.c

---
+++ chapter6/sum.c
@@ -0,0 +1,19 @@
+/* Sumes a series of numbers entered by the user */
+
+#include <stdio.h>
+
+int main(void){
+    int n, sum = 0;
+
+    printf("This program sums a series of integers.\n");
+    printf("Enter integers (0 to terminate): ");
+    scanf("%d", &n);
+    
+    while(n != 0){
+        sum += n;
+        scanf("%d", &n);
+    }
+    printf("%d", sum);
+    
+    return 0;
+}
