adabe7f483891606cbf31cd49af1a5e45d745908
LuluVnB <paraguya.gian@gmail.com>
2025-12-12 16:13:57 -0800

    removed previous logic

---
+++ chapter6/6programmingProject2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b, gcd, temp;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    if(b == 0){
+        gcd = b;
+    } else{
+
+    }
+
+
+    printf("Greatest common divisor: %d", gcd);
+
+    return 0;
+}
+
+
+//greatest common denominator calculator
+//use 22 and 4 as inputs
+//gcd of 22 and 4 is 2.
