66d05270a2f84923526b65e190932d88627160af
LuluVnB <paraguya.gian@gmail.com>
2025-12-12 22:21:33 -0800

    completed and added n0 and d0 variables

--- chapter6/6programmingProject3.c
+++ chapter6/6programmingProject3.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    int n, d, n1, d1, gcd, remainder; //numerator and denominator
+    int n, d, n0, d0, n1, d1, gcd, remainder; //numerator and denominator
     
     printf("Enter a fraction: ");
     scanf("%d/%d", &n, &d);
 
+    n0 = n;
+    d0 = d;
     //find the gcd
     while(1){
         if(d == 0){
             gcd = n;
-            n1 = n/gcd;
-            d1 = d/gcd;
+            n1 = n0/gcd;
+            d1 = d0/gcd;
             printf("In lowest terms: %d/%d\n", n1, d1);
             return 0;
         } else{
