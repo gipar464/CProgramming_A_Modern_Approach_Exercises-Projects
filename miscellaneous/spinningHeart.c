48e1af9b1bf9b0f8f65c97742a27adf793655d21
LuluVnB <paraguya.gian@gmail.com>
2025-12-12 16:27:09 -0800

    turned out to be a spinning donut instead

--- miscellaneous/spinningHeart.c
+++ miscellaneous/spinningHeart.c
@@ -1,36 +1,50 @@
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 int main() {
-    float A=0, B=0;
-    int z[1760], b[1760];
+    float A = 0, B = 0;
+    float i, j;
+    int k;
+    float z[1760];
+    char b[1760];
     printf("\x1b[2J");
     for(;;) {
-        memset(b, 32, 1760*4);
-        memset(z, 0, 1760*4);
-        float t = A * 0.1;
-        float s = 0.9 + 0.1 * sin(B);
-        for(float j=0; j<6.28; j+=0.07) {
-            float ct=cos(t), st=sin(t);
-            for(float i=0; i<6.28; i+=0.02) {
-                float si=sin(i), co=cos(i),
-                      x=16*pow(si,3)*s,
-                      y=13*co-5*co*co-2*co*co*co-x/16,
-                      zv=x*st + y*ct + 5;
-                if(zv>0.02) {
-                    float f=1/(zv), ox=x*f + 40, oy=y*f + 12 + 5*sin(B);
-                    int h=oy*80, idx = (int)(ox+h)%1760;
-                    float l=ct*si*st - co*st + 1;
-                    if(l>.3 && idx<1760 && oy>0 && oy<22 && ox>0 && ox<80)
-                        if(zv>b[idx]) b[idx]=l*8, z[idx]=zv;
+        memset(b, 32, 1760);
+        memset(z, 0, 7040);
+        for(j=0; j < 6.28; j += 0.07) {
+            for(i=0; i < 6.28; i += 0.02) {
+                float c = sin(i);
+                float d = cos(j);
+                float e = sin(A);
+                float f = sin(j);
+                float g = cos(A);
+                float h = d + 2;
+                float D = 1 / (c * h * e + f * g + 5);
+                float l = cos(i);
+                float m = cos(B);
+                float n = sin(B);
+                float t = c * h * g - f * e;
+                int x = 40 + 30 * D * (l * h * m - t * n);
+                int y = 12 + 15 * D * (l * h * n + t * m);
+                int o = x + 80 * y;
+                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
                 }
             }
         }
         printf("\x1b[H");
-        for(int k=0; k<1760; k++)
-            putchar(k%80? " .:-=+*#%@"[(int)b[k]]:10);
-        A+=0.04, B+=0.02;
+        for(k = 0; k < 1761; k++) {
+            putchar(k % 80 ? b[k] : 10);
+            A += 0.00004;
+            B += 0.00002;
+        }
+        A += 0.04;
+        B += 0.02;
         usleep(30000);
     }
+    return 0;
 }
