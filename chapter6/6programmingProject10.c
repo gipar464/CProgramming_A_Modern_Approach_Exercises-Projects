6d8a387441fb1bd9e666f8820a0a2b1f2c063093
LuluVnB <paraguya.gian@gmail.com>
2025-12-14 15:17:18 -0800

    getting closer?

--- chapter6/6programmingProject10.c
+++ chapter6/6programmingProject10.c
@@ -2,70 +2,35 @@
 
 int main(void){
     int month, day, year;
-    int smonth, sday, syear; //s stands for second (second month, etc);
     int earliestMonth, earliestDay, earliestYear;
 
-    while(1){
+    printf("Enter a date (mm/dd/yy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+    earliestMonth = month;
+    earliestDay = day;
+    earliestYear = year;
+
+    while(month != 0 && day != 0 && year != 0){
+        if(earliestYear > year){
+            earliestMonth = month;
+            earliestDay = day;
+            earliestYear = year;
+        }
+        else if(earliestMonth > month){
+            earliestMonth = month;
+            earliestDay = day;
+            earliestYear = year;   
+        }
+        else if(earliestDay > day){
+            earliestMonth = month;
+            earliestDay = day;
+            earliestYear = year;
+        }
         printf("Enter a date (mm/dd/yy): ");
         scanf("%d/%d/%d", &month, &day, &year);
-        if(month == 0 && day == 0 && year == 0){
-            printf("%.2d/%.2d/%.2d is the earliest date.", earliestMonth, earliestDay, earliestYear);
-            return 0;
-        } else{
-            printf("Enter a date (mm/dd/yy): ");
-            scanf("%d/%d/%d", &smonth, &sday, &syear);
-        }
-
-        if(year > syear){
-            earliestMonth = smonth;
-            earliestDay = sday;
-            earliestYear = syear;
-        }
-        else if(year == syear){
-            if(month > smonth){
-                earliestMonth = smonth;
-                earliestDay = sday;
-                earliestYear = syear;
-            }
-        }
     }
 
+    printf("%.2d/%.2d/%.2d is the earliest date.\n", earliestMonth, earliestDay, earliestYear);
+    return 0;
 }
 
-
-
-/*
-
-    if(year < syear){
-        //
-    }
-    else if(year == syear){
-        if(month < smonth){
-            //
-        }
-        else if(month == smonth){
-            if(day < sday){
-                //continue
-            }
-            else if(day == sday){
-                //continue
-            }
-            else if(day > sday){
-                month = smoth;
-                day = sday;
-                year = syear;
-            }
-        }
-        else if(month > smonth){
-            month = smoth;
-            day = sday;
-            year = syear;
-        }
-    }
-    else if(year > syear){
-        month = smoth;
-        day = sday;
-        year = syear;
-    }
-
-*/
