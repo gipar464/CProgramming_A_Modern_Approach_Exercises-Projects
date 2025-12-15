097c8c5886ae2ebf6dc8571a155a74a0238fe3a9
LuluVnB <paraguya.gian@gmail.com>
2025-12-14 17:33:48 -0800

    left off on here for now. will come back to this

--- chapter6/6programmingProject11.c
+++ chapter6/6programmingProject11.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main(void){
-    float e = 1.0f;
-    int n;
+    float n, e = 1.0f;
 
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    for(int i = 1; i <= n; ++i){
+    printf("Enter a number: ");
+    scanf("%f", &n);
+    for(float i = 1.0f; i <= n; ++i){
         e += (1/i); 
     }
 
