fb622f2a8a339a6c9bea3159ae9e0ebbf5e84438
LuluVnB <paraguya.gian@gmail.com>
2025-12-19 12:36:02 -0800

    temp

--- chapter6/6programmingProject11.c
+++ chapter6/6programmingProject11.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    float n, e = 1.0f; 
+    float n, e = 1.0f;
+    int factorial = 1; 
 
     printf("Enter a number: ");
     scanf("%f", &n);
-    for(float i = 1.0f; i <= n; ++i){
-        e += (1/i); 
+
+    for(int i = 0; i < n; ++i){
+        for(int j = 1; j <= factorial; ++j){
+            factorial *= j; //not being stored (always being updated and changing values)
+        }
+        e += 1/factorial;
     }
+
 
     printf("e is approximately %f.\n", e);
     return 0;
