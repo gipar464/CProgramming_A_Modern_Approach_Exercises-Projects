9c7a0bf48eb150c01d32ff470b5f6fcd731c4305
LuluVnB <paraguya.gian@gmail.com>
2025-12-19 22:19:10 -0800

    solved but needed to look at solution

--- chapter6/6programmingProject11.c
+++ chapter6/6programmingProject11.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    float n, e = 1.0f;
-    float factorial = 1.0f; 
+    float e = 1.0f, factorial = 1.0f; 
+    int n;
 
     printf("Enter a number: ");
-    scanf("%f", &n);
-
-    for(float j = n; j > 0; --j){
-        for(int i = 1; i <= n; ++i){
-            factorial *= i;
-        }
-        e += (1/factorial);
+    scanf("%d", &n);
+    
+    for(int i = 1; i <= n; ++i){
+	    factorial *= i;
+	    e += 1.0/factorial;
     }
 
-    printf("e is approximately %f.\n", e);
+
+
+    printf("e is approximately %.10f.\n", e);
     return 0;
 }
