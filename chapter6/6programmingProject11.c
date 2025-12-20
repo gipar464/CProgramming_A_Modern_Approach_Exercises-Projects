8722a2ce8c466431ed16f0b4762e619b7c586970
LuluVnB <paraguya.gian@gmail.com>
2025-12-19 16:39:07 -0800

    Fix factorial calculation in e approximation program
    Corrected the logic for calculating factorial in the loop to ensure accurate computation of the mathematical constant e. Updated variable types and loop structure for proper accumulation.

--- chapter6/6programmingProject11.c
+++ chapter6/6programmingProject11.c
@@ -2,18 +2,17 @@
 
 int main(void){
     float n, e = 1.0f;
-    int factorial = 1; 
+    float factorial = 1.0f; 
 
     printf("Enter a number: ");
     scanf("%f", &n);
 
-    for(int i = 0; i < n; ++i){
-        for(int j = 1; j <= factorial; ++j){
-            factorial *= j; //not being stored (always being updated and changing values)
+    for(float j = n; j > 0; --j){
+        for(int i = 1; i <= n; ++i){
+            factorial *= i;
         }
-        e += 1/factorial;
+        e += (1/factorial);
     }
-
 
     printf("e is approximately %f.\n", e);
     return 0;
