28d04e8f6cac177e9c6489836c7454a03a14ab7f
Gian <paraguya.gian@gmail.com>
2025-12-25 10:14:24 -0800

    solved?

--- chapter7/7programmingProject1.c
+++ chapter7/7programmingProject1.c
@@ -3,14 +3,15 @@
 #include <stdio.h>
 
 int main(void){
-	int i, n;
+	short int i;
+	int n;
 
 	printf("This program prints a table of squares.\n");
 	printf("Enter a number of entries in table: ");
 	scanf("%d", &n);
 
 	for(i =1; i <=n; ++i){
-		printf("%10d%10d\n", i, i * i);
+		printf("%10hd%10hd\n", i, i * i);
 	}
 
 	return 0;
