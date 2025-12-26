c85fd7d8950828621e7b5ce581e70566288be09b
Gian <paraguya.gian@gmail.com>
2025-12-25 16:50:21 -0800

    refactored version

--- chapter7/7programmingProject2.c
+++ chapter7/7programmingProject2.c
@@ -12,11 +12,10 @@ int main(void){
 	for(i = 0; i < n; ++i){
 		printf("%10d%10d\n", i, i * i);
 		if(i % 24 == 0){
-			printf("Press ENTER to continue...");
-			while(getchar() != '\n'){
-				printf("Press ENTER to continue...");
+			do{
+				printf("Press ENTER to continue...\n");
 				getchar();
-			}			
+			} while(getchar() != '\n');	
 		} 
 	}
 
