ed69293e2233aec47dd78f47b78bbee76253f934
Gian <paraguya.gian@gmail.com>
2025-12-27 02:41:35 -0800

    completed

---
+++ chapter7/7programmingProject6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("Size of int: %ld\n", sizeof(int));
+    printf("Size of short: %ld\n", sizeof(short int));
+    printf("Size of long: %ld\n", sizeof(long int));
+    printf("Size of float: %ld\n", sizeof(float));
+    printf("Size of double: %ld\n", sizeof(double));
+    printf("Size of long double: %ld\n", sizeof(long double));
+
+
+    return 0;
+}
