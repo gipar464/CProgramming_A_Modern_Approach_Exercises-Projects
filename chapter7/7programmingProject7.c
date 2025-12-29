06a2b1a063facbb0becf2eaed9be96bd38358523
Gian <paraguya.gian@gmail.com>
2025-12-28 17:53:58 -0800

    100% done

--- chapter7/7programmingProject7.c
+++ chapter7/7programmingProject7.c
@@ -6,30 +6,30 @@
 int main(void){
     int num1, denom1, num2, denom2;
     int resultNum, resultDenom; 
-    char ch; 
+    char operation; 
     
-    printf("Enter two fractions separated by a plus sign: ");
+    printf("Enter your first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+    printf("Enter an operation: (*, /, +, -): ");
+    operation = getchar();
+    printf("Enter your section fraction: ");
+    scanf("%d/%d", &num2, &denom2);
 
-    do{
-        ch = getchar(); 
-                    
-        num1 = ch;
-    } while(ch != '\n');
-
-    if(ch == '+'){
-        scanf("%d/%d+%d/%d", &num1, &denom1, &num2, &denom2);
+    if(operation == '+'){
         resultNum = (num1 * denom2) + (denom1 * num2);
         resultDenom = denom1 * denom2;
     }
-    else if(ch == '-'){
-        scanf("%d/%d-%d/%d", &num1, &denom1, &num2, &denom2);
+    else if(operation == '-'){
         resultNum = (num1 * denom2) - (denom1 * num2);
         resultDenom = denom1 * denom2;
     }  
-    else if(ch == '*'){
-        scanf("%d/%d*%d/%d", &num1, &denom1, &num2, &denom2);
+    else if(operation == '*'){
         resultNum = num1 * num2;
         resultDenom = denom1 * denom2; 
+    }
+    else if(operation == '/'){
+        resultNum = num1 * denom2;
+        resultDenom = denom1 * num2;
     }
     
     printf("\n");
