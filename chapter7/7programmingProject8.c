9eb93cc25f221fb33e53f381990745573c57c08d
Gian <paraguya.gian@gmail.com>
2025-12-30 00:47:08 -0800

    solved

--- chapter7/7programmingProject8.c
+++ chapter7/7programmingProject8.c
@@ -5,8 +5,21 @@
 #include <ctype.h>
 
 int main(void){
-    int hours, minutes;    
+    int hours, minutes;   
+    int convertedTime;
     char timeSpecification; 
+    int departureTime1, departureTime2, departureTime3, departureTime4, departureTime5, departureTime6, departureTime7, departureTime8; 
+
+    departureTime1 = (8 * 60);
+    departureTime2 = (9 * 60) + 43;
+    departureTime3 = (11 * 60) + 19;
+    departureTime4 = (12 * 60) + 47;
+    departureTime5 = (14 * 60);
+    departureTime6 = (15 * 60) + 45;
+    departureTime7 = (19 * 60);
+    departureTime8 = (21 * 60) + 45;
+
+
     do {
         printf("Enter a time: ");
         scanf("%d:%d", &hours, &minutes);
@@ -16,15 +29,40 @@ int main(void){
             scanf("%d:%d", &hours, &minutes);
         } else{
             do{
+                convertedTime = (hours * 60) + minutes; //converted time used for calculations
                 timeSpecification = getchar();
                 switch(timeSpecification){
                     case 'A':
                     case 'a':
-                        printf("Time is in the morning.\n");
+                        convertedTime = (hours * 60) + minutes;
+                        if(convertedTime < (departureTime2 + departureTime1)/2){
+                            printf("Closest departure time is 8:00 a.m., arriving 10:16 a.m.\n");
+                        }
+                        else if(convertedTime < (departureTime3 + departureTime2)/2){
+                            printf("Closest departure time is 9:43 a.m., arriving 11:52 a.m.\n");
+                        }
+                        else if(convertedTime < (departureTime4 + departureTime3)/2){
+                            printf("Closest departure time is 11:19 a.m., arriving 1:31 p.m.\n");
+                        }
+                        else if(convertedTime < (departureTime5 + departureTime4)/2){
+                            printf("Closest departure time is 12:47 p.m., arriving 3:00 p.m.\n");
+                        }
                         return 0;
                     case 'P':
                     case 'p':
-                        printf("Time is in the evening.\n");
+                        convertedTime = (12 * 60) + (hours * 60) + minutes;
+                        if(convertedTime < (departureTime6 + departureTime5)/2){
+                            printf("Closest departure time is 2:00 p.m., arriving 4:08 p.m.\n");
+                        }
+                        else if(convertedTime < (departureTime7 + departureTime6)/2){
+                            printf("Closest departure time is 3:45 p.m., arriving 5:55 p.m.\n");
+                        }
+                        else if(convertedTime < (departureTime8 + departureTime7)/2){
+                            printf("Closest departure time is 7:00 p.m., arriving 9:20 p.m.\n");
+                        }
+                        else if(convertedTime > (departureTime8 + departureTime7)/2){
+                            printf("Closest departure time is 9:45 p.m., arriving 11:58 p.m.\n");
+                        }
                         return 0;
                 }
 
