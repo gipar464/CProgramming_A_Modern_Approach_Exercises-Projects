8c4662d0f2ba13011d3937e803c1cb8e92a8eb19
Gian <paraguya.gian@gmail.com>
2026-01-02 11:29:51 -0800

    tested ascii integer values

---
+++ chapter7/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+    int intValue; 
+    printf("Enter a string: ");
+    intValue = getchar(); 
+
+    printf("The int value of your string is: %d", intValue); 
+    
+    return 0;
+}
