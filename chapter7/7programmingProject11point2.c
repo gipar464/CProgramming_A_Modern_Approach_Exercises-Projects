22f6efacefd6964a8f001dd75a51992f12dd0d01
Gian <paraguya.gian@gmail.com>
2026-01-02 20:29:22 -0800

    solved

--- chapter7/7programmingProject11point2.c
+++ chapter7/7programmingProject11point2.c
@@ -1,19 +1,36 @@
 /* 01/02/2026 */
 
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void){
-    char letter, checker;
+    char firstNameLetter, lastNameLetter, checker;
     
     printf("Please enter a first and a last name: ");
     
     do{
         scanf("%c", &checker);  //checks if letter scanned is white space or not. if yes, then it won't print anything 
         if(checker != ' '){
-            letter = checker;
-            printf("%c", letter);
+            firstNameLetter = checker;
+            break;
         }
-    } while(letter != '\n');
+    } while(1);
+    
+    while(getchar() != ' '); //skips the rest of the letters in the first name until the blank space is reached 
 
-    return 0;
+
+    do{
+        scanf("%c", &checker);
+        if(checker != ' ' && checker != '\n'){
+            lastNameLetter = checker;
+            putchar(lastNameLetter);
+        }
+        
+        checker = toupper(checker); 
+        if((checker < 'A' || checker > 'Z') && checker != ' '){
+            printf(", %c.", firstNameLetter);
+            return 0;
+        }
+    } while(1);
+    
 }
