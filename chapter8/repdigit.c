ddd8842aeca95b990a57f483a03a05a098be5eb6
Gian <paraguya.gian@gmail.com>
2026-01-04 15:06:48 -0800

    from book

---
+++ chapter8/repdigit.c
@@ -0,0 +1,32 @@
+/* Checks numbers for repeated digits 
+ * 01/04/2026
+ */
+
+#include <stdio.h>
+#include <stdbool.h> /* C99 only */
+
+int main(void){
+    bool digit_seen[10] = {false};
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while(n > 0){
+        digit = n % 10;
+        if(digit_seen[digit]){
+            break;
+        }
+        digit_seen[digit] = true;
+        n /= 10;
+    }
+
+    if(n > 0){
+        printf("Repeated digit\n");
+    }else{
+        printf("No repeated digit\n");
+    }
+
+    return 0;
+}
