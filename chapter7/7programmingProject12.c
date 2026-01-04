97d6057c5e287179b9a136de50066476977be660
Gian <paraguya.gian@gmail.com>
2026-01-04 00:47:01 -0800

    refactored to only handle three operands and two operations

--- chapter7/7programmingProject12.c
+++ chapter7/7programmingProject12.c
@@ -1,32 +1,47 @@
 /* 12/31/2025 */
+// the problem in this problem here is that it will only work for three operands and two operators. in the future, i will implement an infinite loop that allows the user to enter and amount of operators and operands as they wish.
+
 
 #include <stdio.h>
 #include <ctype.h>
 
 int main(void){
-    float o1, o2, result = 0.0f; //o1 & o2 == operand1 and operand2, respectively 
-    char operation;
+    float operand1, operand2, operand3, result = 0.0f; 
+    char operator1, operator2;
 
     printf("Enter an expression: ");
-    do{
-        scanf("%f", &o1);
-        scanf("%c", &operation);
-        scanf("%f", &o2);
-        if(operation == '*'){
-            result += o1 * o2;    
-        }
-        else if(operation == '/'){
-            result += o1 / o2;
-        } 
-        else if(operation == '+'){
-            result += o1 + o2;
-        }
-        else if(operation == '-'){
-            result += o1 - o2;
-        }
-    } while(getchar() != '\n');
-
-    printf("Value of epxression: %f", result);
+    scanf("%f%c%f%c%f", operand1, operator1, operand2, operator2, operand3);
     
+    switch(operator1){
+        case '*':
+            result = operand1 * operand2;
+            break;
+        case '/':
+            result += operand1 / operand2;
+            break;
+        case '+':
+            result += operand1 + operand2;
+            break;
+        case '-':
+            result += operand1 - operand2;
+            break;
+    }
+
+    switch(operator2){
+        case '*':
+            result = result * operand3;
+            break;
+        case '/':
+            result = result / operand3;
+            break;
+        case '+':
+            result = result + operand3;
+            break;
+        case '-':
+            result = result - operand3;
+            break;
+    }
+
+    printf("Value of epxression: %f\n", result);
     return 0;
 }
