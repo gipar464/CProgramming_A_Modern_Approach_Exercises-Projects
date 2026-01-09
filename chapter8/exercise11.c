16a483c296e5e6ed88f2597bbfeecbbd7c1a74a9
Gian <paraguya.gian@gmail.com>
2026-01-09 15:13:18 -0800

    added spacing

--- chapter8/exercise11.c
+++ chapter8/exercise11.c
@@ -8,7 +8,7 @@ int main(void){
     for(int i = 0; i < 8; ++i){
         for(int j = 0; j < 8; ++j){
             checker_board[i][j] = '4'; 
-            printf("%c", checker_board[i][j]);
+            printf(" %c", checker_board[i][j]);
         }
         printf("\n");
     }
