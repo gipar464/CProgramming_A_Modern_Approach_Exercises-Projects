7349e1924ac452d0fc699dbecf454f06ff80c95a
Gian <paraguya.gian@gmail.com>
2026-01-09 15:15:38 -0800

    added alternate characters

--- chapter8/exercise11.c
+++ chapter8/exercise11.c
@@ -7,7 +7,12 @@ int main(void){
 
     for(int i = 0; i < 8; ++i){
         for(int j = 0; j < 8; ++j){
-            checker_board[i][j] = '4'; 
+            if(j % 2 == 0){
+                checker_board[i][j] = '4';
+            }
+            else{
+                checker_board[i][j] = '6';
+            }
             printf(" %c", checker_board[i][j]);
         }
         printf("\n");
